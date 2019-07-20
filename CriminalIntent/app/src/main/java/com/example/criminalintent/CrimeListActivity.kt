af428b5d5ff3091d65a0437afef3c3f728d9fa6f
Your Name <you@example.com>
2019-07-20 11:59:43 +0900

    CrimeListFragment add

---
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListActivity.kt
@@ -0,0 +1,9 @@
+package com.example.criminalintent
+
+import androidx.fragment.app.Fragment
+
+class CrimeListActivity : SingleFragmentActivity(){
+    override fun createFragment(): Fragment {
+        return CrimeListFragment()
+    }
+}
