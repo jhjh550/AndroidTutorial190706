d021049d2a0fdfbe7234db590c3bb7dbb4977939
Your Name <you@example.com>
2019-07-20 11:27:50 +0900

    SingleFragmentActivity add

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeActivity.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeActivity.kt
@@ -2,20 +2,10 @@ package com.example.criminalintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
-class CrimeActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fragment)
-
-        var fragment =
-            supportFragmentManager.findFragmentById(R.id.fragment_container)
-
-        if(fragment == null){
-            fragment = CrimeFragment()
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container, fragment)
-                .commit()
-        }
+class CrimeActivity : SingleFragmentActivity() {
+    override fun createFragment(): Fragment {
+        return CrimeFragment()
     }
 }
