6cd166edd72c46cca8c789bd557689e9ada8e91a
Your Name <you@example.com>
2019-07-27 15:47:35 +0900

    service conn

--- Service/app/src/main/java/com/example/service/MainActivity.kt
+++ Service/app/src/main/java/com/example/service/MainActivity.kt
@@ -2,22 +2,27 @@ package com.example.service
 
 import android.app.Service
 import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     var myService: MyService? = null
+    var isBound = false
     val myConnection = object : ServiceConnection{
         override fun onServiceDisconnected(className: ComponentName?) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            isBound = false
         }
         override fun onServiceConnected(className: ComponentName?, service: IBinder?) {
             val binder = service as MyService.MyLocalBinder
             myService = binder.getService()
+            isBound = true
         }
     }
 
@@ -26,11 +31,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val intent = Intent(this, MyService::class.java)
-        bindService(intent, )
+        bindService(intent, myConnection, Context.BIND_AUTO_CREATE)
+
+        btnNumber.setOnClickListener {
+            Log.i("service", "num: "+myService?.getMyNumber())
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        unbindService()
+        if(isBound) {
+            unbindService(myConnection)
+        }
     }
 }
